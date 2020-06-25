5422daee96790568b86a89417a88e5bd073664ac
Kubilay <kblycaglayan@gmail.com>
2020-06-25 18:27:17 +0300

    Hide buttons when not using

--- assets/js/DOM.js
+++ assets/js/DOM.js
@@ -1,5 +1,5 @@
 const GameDisplay = (() => {
-  const submitButton = document.getElementById('submit');
+  const startButton = document.getElementById('submit');
   const playButton = document.getElementById('playAgain');
   const resetButton = document.getElementById('reset');
   const cells = document.querySelectorAll('cell');
@@ -8,10 +8,26 @@ const GameDisplay = (() => {
   const player1score = document.getElementById('play2name');
   const player2score = document.getElementById('play2score');
   const message = document.getElementById('message');
+  const form = document.getElementById('form');
+  const form2 = document.getElementById('form2');
+  const buttonWrapper = document.querySelector('.button-wrapper');
 
   const displayButtons = function displayButtons() {
+    const player1 = document.getElementById('player1').value;
+    const player2 = document.getElementById('player2').value;
 
+    if (player1.length > 0 && player2.length > 0) {
+      buttonWrapper.style.display = 'block';
+      form.style.display = 'none';
+    }
   };
+
+  const startGame = function startGame() {
+    displayButtons();
+    GamePlay.gameStatus = true;
+  };
+
+  startButton.addEventListener('click', startGame);
 
   const displayMessage = function displayMessage() {
 
